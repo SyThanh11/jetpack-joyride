3ed8d4918e60443ecc9b631c2caa4dcfe1e5eefc
Thanh Nguyen <nguyensythanh11@gmail.com>
2024-07-03 21:06:00 +0700

    Add new map and fix missile

--- src/game.ts
+++ src/game.ts
@@ -18,7 +18,7 @@ const config: Types.Core.GameConfig = {
     physics: {
         default: 'arcade',
         arcade: {
-            // debug: true,
+            debug: true,
         },
     },
     scene: [BootGameScene, PreloadScene, MainGame, PauseScene],
